28dcdc1650f64e88c93e8e2b3446118054254e38
Zulqarnain haier <123175331+Zulqar3203060380@users.noreply.github.com>
2024-07-28 04:46:05 -0700

    working On this project

---
+++ firebase.js
@@ -0,0 +1,16 @@
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA6JaNS5ZFT62-3YO-eRq2AuIfQ4Yto4do",
+    authDomain: "ordinary-task-a09fd.firebaseapp.com",
+    projectId: "ordinary-task-a09fd",
+    storageBucket: "ordinary-task-a09fd.appspot.com",
+    messagingSenderId: "666767135653",
+    appId: "1:666767135653:web:2d2d8b1e87503df60b0e4c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
